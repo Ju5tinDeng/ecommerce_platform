634b98d43949e92baf4c857834b8fd1bc033ff6b
justin <justinx0505@gmail.com>
2024-07-17 10:06:45 +0800

    feat: Add Order and OrderItem models with associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_07_10_182027) do
+ActiveRecord::Schema[7.0].define(version: 2024_07_17_015906) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,6 +22,24 @@ ActiveRecord::Schema[7.0].define(version: 2024_07_10_182027) do
     t.index ["product_id"], name: "index_cart_items_on_product_id"
   end
 
+  create_table "order_items", force: :cascade do |t|
+    t.bigint "order_id", null: false
+    t.string "product_name"
+    t.integer "product_quantity"
+    t.decimal "unit_price"
+    t.decimal "subtotal_price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_order_items_on_order_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string "order_number"
+    t.decimal "total_price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.text "description"
@@ -31,4 +49,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_07_10_182027) do
   end
 
   add_foreign_key "cart_items", "products"
+  add_foreign_key "order_items", "orders"
 end
